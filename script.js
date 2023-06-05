504e2b06201a3550e9fa7136db46b4bd10fe7e6b
Yugandhar2007 <106568728+Yugandhar2007@users.noreply.github.com>
2023-06-05 21:45:03 +0530

    performed all DOM manupulations

--- script.js
+++ script.js
@@ -12,4 +12,6 @@ var changemypic = function(){
         isdata1=true;
     }
     document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("myname").innerHTML=displaydata.name;
+    document.getElementById("mydesig").innerHTML=displaydata.designation;
 }
